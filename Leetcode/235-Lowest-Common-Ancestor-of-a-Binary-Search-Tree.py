7e5780921d3b57c017b383d54aca10d7e88a1cf2
Bedru umer <bedru2019@gmail.com>
2023-03-29 20:53:57 +0200

    update

--- Leetcode/235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
+++ Leetcode/235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree.py
@@ -7,43 +7,11 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        parents = {}
-        
-        queue = collections.deque()
-        queue.append([root, None])
-        while queue:
-            curr, parent = queue.popleft()
-            parents[curr] = parent
-            if curr.left: queue.append([curr.left, curr])
-            if curr.right: queue.append([curr.right, curr])
-        
-        pparents = [p]
-        ptemp = p
-        while parents[ptemp]:
-            pparents.append(parents[ptemp])
-            ptemp = parents[ptemp]
-        
-        qparents = [q.val]
-        qtemp = q
-        while parents[qtemp]:
-            qparents.append(parents[qtemp].val)
-            qtemp = parents[qtemp]
-            
-        qparents.sort()
-        for par in pparents:
-            if self.binarySearch(qparents, par.val): return par
-            
-        return p
-    
-    def binarySearch(self, array, target):
-        start = 0
-        end = len(array) - 1
-        while start <= end:
-            mid = (start + end) // 2
-            if array[mid] == target:
-                return True
-            elif array[mid] > target:
-                end = mid - 1
-            else: start = mid + 1
-        
-        return False
+        if not root: return p if p.left == q or p.right == q else q
+        if p.val < root.val < q.val or q.val < root.val < p.val:
+            return root
+        elif root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif root.val < p.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return root
